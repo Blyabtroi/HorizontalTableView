629ced64609ef7fee762ae8839329cc090b560c1
Basil <blyabtroi@gmail.com>
2015-12-24 11:54:24 +0300

    no message

--- src/Horizontal TableView/Horizontal TableViewTests/Horizontal_TableViewTests.m
+++ src/Horizontal TableView/Horizontal TableViewTests/Horizontal_TableViewTests.m
@@ -27,13 +27,8 @@
 - (void)testExample {
     // This is an example of a functional test case.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
+    XCTAssert(1==1, @"passed");
 }
 
-- (void)testPerformanceExample {
-    // This is an example of a performance test case.
-    [self measureBlock:^{
-        // Put the code you want to measure the time of here.
-    }];
-}
 
 @end
