c71d11f2139abce92b3b7838fef85f79a1074386
Basil <blyabtroi@gmail.com>
2015-12-24 12:27:11 +0300

    no message

--- src/Horizontal TableView/Horizontal TableViewUITests/Horizontal_TableViewUITests.m
+++ src/Horizontal TableView/Horizontal TableViewUITests/Horizontal_TableViewUITests.m
@@ -10,6 +10,10 @@
 
 @interface Horizontal_TableViewUITests : XCTestCase
 
+@property (strong, nonatomic) XCUIApplication *app;
+
+- (void)testExample;
+
 @end
 
 @implementation Horizontal_TableViewUITests
@@ -22,7 +26,8 @@
     // In UI tests it is usually best to stop immediately when a failure occurs.
     self.continueAfterFailure = NO;
     // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-    [[[XCUIApplication alloc] init] launch];
+    self.app = [[XCUIApplication alloc] init];
+    [self.app launch];
     
     // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
 }
@@ -35,7 +40,9 @@
 - (void)testExample {
     // Use recording to get started writing UI tests.
     // Use XCTAssert and related functions to verify your tests produce the correct results.
-    XCTAssert(1==1, @"passed");
+    
+    XCTAssert([self.app.tables.cells count] > 0, @"passed");
+
 }
 
 @end
